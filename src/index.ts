eeba90cc039a16d36ec78b0b871f608a63cea483
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-04 20:14:35 +0545

    updating index file

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 import dotenv from "dotenv";
-import express, {Request,Response}  from "express";
+import express, {Application, Request,Response}  from "express";
 import bodyParser from "body-parser";
+import { connectDatabase } from "./database/mangodb";
+import { PORT } from "./configs";
+import authRouter from "./routes/auth.routes"; 
+import { success } from "zod";
 
-dotenv.config();
+const app: Application = express();
 
-const port = process.env.PORT
-
-const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.use("/api/auth",authRouter);
 app.get("/",(req:Request,res: Response) => {
-    res.send("Hello world")
+    res.status(200).json({success: true, message: "Welcome to API"})
 })
 
 async function startServer(){
+    await connectDatabase();
     app.listen(
-        port,
+        PORT,
         ()=>{
-            console.log(`Server start: http://localhost:${port}`)
+            console.log(`Server start: http://localhost:${PORT}`)
         }
 
     )
