267727e1a8a2d24deffa1054b1f20150c0345182
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-28 20:34:09 +0545

    adding consumer dto

---
+++ src/dtos/consumer.profile.dto.ts
@@ -0,0 +1,38 @@
+import { ProfileData, ProfileSchema } from "../types/consumer.profile";
+import z from "zod";
+
+// export const getProfileDto = ProfileSchema.pick({
+//   userId: true,
+// });
+
+// export type GetProfileDto = z.infer<typeof getProfileDto >;
+
+
+export const updateProfileDto = ProfileSchema.pick({
+  fullName: true,
+  email: true,
+  phoneNumber: true,
+  userLocation: true,
+  profile_image:true
+}).partial({
+  fullName: true,
+  email: true,
+  phoneNumber: true,
+  userLocation: true,
+  profile_image:true
+});
+
+export type UpdateprofileDto = z.infer<typeof updateProfileDto>;
+
+export const createProfileDto = ProfileSchema.pick({
+  userId: true,
+  fullName: true,
+  email: true,
+  phoneNumber: true,
+  userLocation: true,
+}).partial({
+  phoneNumber: true,
+  userLocation: true,
+});
+
+export type CreateprofileDto = z.infer<typeof createProfileDto>;
