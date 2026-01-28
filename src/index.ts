4e1b3bb66a6c70174456ec3667f2992eb28cb247
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-01-28 21:45:51 +0545

    changes on index file

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ import bodyParser from "body-parser";
 import { connectDatabase } from "./database/mangodb";
 import { PORT } from "./configs";
 import authRouter from "./routes/auth.routes"; 
+import consumerProfileRouter from "./routes/consumer.profile.route";
 import cors from 'cors'
+import path from "path";
 
 const app: Application = express();
 
@@ -19,8 +21,12 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use("/uploads",express.static(path.join(__dirname,'../uploads')));
+
+
 
 app.use("/api/auth",authRouter);
+app.use("/api/consumer",consumerProfileRouter)
 app.get("/",(req:Request,res: Response) => {
     res.status(200).json({success: true, message: "Welcome to API"})
 })
