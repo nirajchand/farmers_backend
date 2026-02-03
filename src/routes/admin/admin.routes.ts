265d158abb3b3b489a6c2df6444f99fedf6ff220
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-02-03 17:35:04 +0545

    adding admin routes

---
+++ src/routes/admin/admin.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { adminMiddleware, authorizedMiddleware } from "../../middlewares/auth.middleware";
+import { uploads } from "../../middlewares/upload.middleware";
+import { AdminUserController } from "../../controllers/admin/admin.controller";
+
+let adminUserController = new AdminUserController
+
+const router = Router()
+
+router.use(authorizedMiddleware);
+router.use(adminMiddleware);
+
+router.post("/", uploads.single("profile_image"), adminUserController.createUser);
+router.get("/", adminUserController.getAllUsers);
+router.put("/:id", uploads.single("profile_image"), adminUserController.updateUser);
+router.delete("/:id", adminUserController.deleteUser);
+router.get("/:id", adminUserController.getUserById);
+
+export default router;
