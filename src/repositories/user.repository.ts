b0812d1dfe5be3760dccd8477896bf3d66021d24
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-02-03 17:34:20 +0545

    adding changes om repo

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -4,10 +4,25 @@ export interface IUserRepository{
     createUser(userData: Partial<IUser>) : Promise<IUser>;
     getUserByEmail(email: string): Promise<IUser | null>;
     findUserById(id: String): Promise<IUser | null>;
+    updateUser(id: String,newData: Partial<IUser>): Promise<IUser | null>;
+    getAllUsers():Promise<IUser[]>;
+    deleteUser(id: string): Promise<boolean>;
 }
 
 
 export class UserRepository implements IUserRepository{
+    async deleteUser(id: string): Promise<boolean> {
+        const result = await UserModel.findByIdAndDelete(id);
+        return result? true: false;
+    }
+    async getAllUsers(): Promise<IUser[]> {
+        const users  = await UserModel.find();
+        return users;
+    }
+    async updateUser(id: String, newData: Partial<IUser>): Promise<IUser | null> {
+        const user = await UserModel.findByIdAndUpdate(id,newData,{new : true});
+        return user;
+    }
 
     async createUser(userData: Partial<IUser>): Promise<IUser> {
         const user = new UserModel(userData);
