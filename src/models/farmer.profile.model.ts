123399802435b3134a62d8bc8fa82921b8dcb310
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-02-14 00:25:31 +0545

    adding model for farmer profile

---
+++ src/models/farmer.profile.model.ts
@@ -0,0 +1,29 @@
+import { FarmerProfile } from "../types/farmer.profile";
+import mongoose,{Schema} from "mongoose";
+
+
+export interface IFarmerProfile extends Omit<FarmerProfile,"userId"> , Document{
+    "userId": mongoose.Types.ObjectId | string,
+    _id: mongoose.Types.ObjectId,
+    createdAt:Date,
+    updateAt:Date
+}
+
+
+const FarmerProfileSchema: Schema = new Schema<IFarmerProfile>(
+    {
+        userId: {type: Schema.Types.ObjectId, ref: "Users", required: true},
+        email: {type: String, required: true},
+        fullName: {type: String, required: true},
+        farmName: {type: String},
+        description: {type: String},
+        farmLocation: {type: String},
+        phoneNumber: {type: String},
+        profile_image: {type: String},
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const FarmerProfileModel = mongoose.model<IFarmerProfile>("Farmer_Profile", FarmerProfileSchema)
