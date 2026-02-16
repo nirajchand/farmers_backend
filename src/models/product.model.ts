193b2d7db2c4a729b575129cdc911ebc7e7b6736
nirajchand <149762449+nirajchand@users.noreply.github.com>
2026-02-16 14:32:40 +0545

    adding products model

---
+++ src/models/product.model.ts
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+import { ProductType } from "../types/product";
+
+export interface IProductModel extends Omit<ProductType, "farmerId">, Document {
+  farmerId: mongoose.Types.ObjectId | string;
+  _id: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const productSchema: Schema = new Schema<IProductModel>(
+  {
+    farmerId: {
+      type: Schema.Types.ObjectId,
+      ref: "Farmer_Profile",
+      required: true,
+    },
+
+    productName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    unitType: {
+      type: String,
+      required: true,
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    product_image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ProductModel = mongoose.model<IProductModel>("Products", productSchema);
